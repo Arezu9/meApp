c9c0908d4e2511bc0b32f53ed628053c15863ae7
Arezoo <arezooinfo883@gmail.com>
2024-07-26 13:29:30 +0100

    Initial Commit

---
+++ meApp/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  meApp
+//
+//  Created by Scholar on 25/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("topbit")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+
+            Spacer()
+            
+            HStack {
+                VStack {
+                    
+                    Image ("babyme")  .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(16.0)
+                    
+                }
+                VStack {
+                    Image("meee")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(23.0)
+                    Image("aaa")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(29.0)
+                    
+                    
+                    
+                }
+                
+            
+        
+            }
+            
+            Image("keyboard2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ meApp/meAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  meAppApp.swift
+//  meApp
+//
+//  Created by Scholar on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct meAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
